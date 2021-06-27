90e376223f8c7519ea457540db6a62b80208b68d
BenniTec <benni@stuerz.xyz>
2021-06-27 21:52:25 +0200

    improved bash completion

--- util/bcc.bash
+++ util/bcc.bash
@@ -13,6 +13,10 @@ _bcc() {
    $split && return
 
    if [[ $cur == -* ]]; then
+      if [[ $cur == -O* ]]; then
+         COMPREPLY=($(compgen -W '-O0 -O1 -O2 -O3' -- "$cur"))
+         return
+      fi
       COMPREPLY=($(compgen -W '$(_parse_help "$1" -h)' -- "$cur"))
       [[ ${COMPREPLY-} == *= ]] && compopt -o nospace
       return
