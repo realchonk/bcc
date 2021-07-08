56933ace52ee9c948d35a5fbd789e7fbbd076b59
BenniTec <benni@stuerz.xyz>
2021-07-08 14:21:10 +0200

    continued pre-processor

--- cpp/src/main.c
+++ cpp/src/main.c
@@ -1,9 +1,12 @@
 #include <unistd.h>
+#include <string.h>
 #include <stdio.h>
+#include <errno.h>
 
 int main(int argc, char* argv[]) {
+   const char* output_name = "-";
    int option;
-   while ((option = getopt(argc, argv, ":D:VE")) != -1) {
+   while ((option = getopt(argc, argv, ":D:VEo:")) != -1) {
       switch (option) {
       case 'V':
          puts("bcpp " BCC_VER);
@@ -20,6 +23,9 @@ int main(int argc, char* argv[]) {
       case '?':
          fprintf(stderr, "bcpp: invalid option '-%c'\n", optopt);
          return 1;
+      case 'o':
+         output_name = optarg;
+         break;
       default:
          goto print_usage;
       }
@@ -29,5 +35,24 @@ int main(int argc, char* argv[]) {
       fputs("Usage: bcpp [options] input\n", stderr);
       return 1;
    }
+
+   const char* source_name = argv[optind];
+   FILE* source = !strcmp(source_name, "-") ? stdin : fopen(source_name, "r");
+   if (!source) {
+      fprintf(stderr, "bcpp: failed to open '%s': %s\n", source_name, strerror(errno));
+      return 1;
+   }
+
+   FILE* output = !strcmp(output_name, "-") ? stdout : fopen(output_name, "w");
+   if (!output) {
+      fprintf(stderr, "bcpp: failed to open '%s': %s\n", output_name, strerror(errno));
+      fclose(source);
+      return 1;
+   }
+
+   // TODO: implement
+
+   fclose(source);
+   fclose(output);
    return 0;
 }
