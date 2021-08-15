9ab3ac6d5892c63aafa5612e54cffe9c2511a037
BenniTec <benni@stuerz.xyz>
2021-08-16 00:34:36 +0200

    cleanup after release

--- cpp/help_options.h
+++
@@ -1,9 +0,0 @@
-const char* help_options =
-	"  -o                   Specify the output file (or '-' for standard output). By default the output file is '-'.\n"
-	"  -I                   Specify an include path for the preprocessor to search.\n"
-	"  -E                   Ignored, since this is the preprocessor.\n"
-	"  -C                   Suppress the color output.\n"
-	"  -V                   Print the version information.\n"
-	"  -h                   Print a simple help message.\n"
-	"  -D                   Specify a predefined macro.\n"
-;
