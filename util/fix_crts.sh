c12a995ebf3ad8470f7be02197b83b97d9aca1b2
BenniTec <benni@stuerz.xyz>
2021-08-29 15:50:45 +0200

    improved fix_crts.sh

--- util/fix_crts.sh
+++ util/fix_crts.sh
@@ -9,21 +9,26 @@ error() {
    echo "$(basename "$0"): $1" >&2
    exit 1
 }
+gcc_find() {
+   file="$("${GCC}" -print-file-name="$1")"
+   [ "${file}" = "$1" ] && error "failed to find '$1'"
+   echo "${file}"
+}
 fix() {
    if [ ! -e "${libdir}/$1" -o "${force}" = "1" ]; then
-      ln -sfv "$("${GCC}" -print-file-name="$1")" "${libdir}/$1" || exit 1
+      ln -sfv "$(gcc_find "$1")" "${libdir}/$1" || exit 1
       fixed="${fixed} $1"
    fi
 }
 fix_clib() {
    if [ ! -e "${clibdir}/$1" -o "${force}" = "1" ]; then
-      ln -sfv "$("${GCC}" -print-file-name="$1")" "${clibdir}/$1" || exit 1
+      ln -sfv "$(gcc_find "$1")" "${clibdir}/$1" || exit 1
       fixed="${fixed} $1"
    fi
 }
 
 
-args=$(getopt -o "ahg:t:f" -- "$@") || exit 1
+args=$(getopt -o "ahg:t:fF:" -- "$@") || exit 1
 eval set -- "${args}"
 unset args
 
@@ -31,24 +36,30 @@ target="${MACHTYPE}"
 GCC="${MACHTYPE}-gcc"
 force=0
 fix_all=0
+search_files=""
 
 while true; do
    case "$1" in
    -h)
       echo "Usage: $(basename "$0") [options] prefix"
+      echo
       echo "This script fixes missing crtX.o and libc.a files."
+      echo
       echo "Options:"
-      echo "  -h           Show this page"
+      echo "  -h           Show this page."
       echo "  -a           Fix also crtbegin.o & crtend.o"
-      echo "               use this only if linking against glibc fails"
-      echo "  -f           Overwrite already existing files"
-      echo "  -t target    Specify the target of bcc"
-      echo "  -g GCC       Specify the name of gcc"
+      echo "               use this only if linking against glibc fails."
+      echo "  -f           Overwrite already existing files."
+      echo "  -F file      Only print the path to file."
+      echo "               (may be specified multiple times)"
+      echo "  -t target    Specify the target of bcc."
+      echo "  -g GCC       Specify the name of gcc."
       echo
       echo "Operands:"
       echo "  prefix       The installation prefix of bcc"
       echo "  target       The target of bcc (default: ${MACHTYPE})"
       echo "  GCC          The name of gcc (default: ${MACHTYPE}-gcc)"
+      echo "  file         A file or files to be searched by gcc"
       exit 0
       ;;
    -a)
@@ -58,6 +69,10 @@ while true; do
    -f)
       force=1
       shift
+      ;;
+   -F)
+      search_files="${search_files} $2"
+      shift 2
       ;;
    -t)
       target="$2"
@@ -77,6 +92,13 @@ while true; do
       ;;
    esac
 done
+
+if [ -n "${search_files}" ]; then
+   for f in ${search_files}; do
+      gcc_find "${f}"
+   done
+   exit 0
+fi
 
 [ -z "$1" ] && error "operand prefix is missing"
 prefix="$1"
