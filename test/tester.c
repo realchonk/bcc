772faba56d7d5bb7a90e7e3191fc169db1678898
BenniTec <benni@stuerz.xyz>
2021-09-07 17:59:21 +0200

    updated TODO; removed _GNU_SOURCE from test

--- test/tester.c
+++ test/tester.c
@@ -13,7 +13,6 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#define _GNU_SOURCE
 #include <stdnoreturn.h>
 #include <sys/types.h>
 #include <sys/wait.h>
