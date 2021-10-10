36b2e77fbe86f373be14822da9d7ed6ff3f2dcae
Benjamin Stürz <benni@stuerz.xyz>
2021-10-10 15:26:46 +0200

    riscv32: fixed detect_sys.sh

--- src/riscv/detect_sys.sh
+++ src/riscv/detect_sys.sh
@@ -4,11 +4,37 @@ arch="$1"
 cpu="$2"
 
 # determine the CPU by looking at /proc/cpuinfo
-out_cpu="$(cat /proc/cpuinfo | grep 'isa\s*:\s*rv' | head -n1 | sed 's/^.*\(rv\(32|64\).\+\)$/\1/')"
+out_cpu="$(cat /proc/cpuinfo | grep 'isa\s*:\s*rv' | head -n1  | sed 's/^.*:\s*\(.*\)$/\1/')"
 
-# determine the ABI by looking at the Flags entry in the libc.so.6 file
-out_abi=$(readelf -h "$(gcc -print-file-name=libc.so.6)" \
-         | grep 'Flags'                                  \
-         | sed 's/^.*\([a-z]\+\)-float ABI.*$/\1/')
+
+# determine the floating-pont ABI by looking at the Flags entry in the libc.so.6 file
+tmp_abi="$(readelf -h "$(gcc -print-file-name=libc.so.6)"   \
+         | grep 'Flags'                                     \
+         | grep -o '\(soft\|single\|double\|\)-float')"
+
+# determine the base ABI
+case "${arch}" in
+riscv32)
+   base_abi="ilp32"
+   ;;
+riscv64)
+   base_abi="lp64"
+   ;;
+esac
+
+# determine the suffix for the ABI
+case "${tmp_abi}" in
+soft-float)
+   abi_suffix=""
+   ;;
+single-float)
+   abi_suffix="f"
+   ;;
+double-float)
+   abi_suffix="d"
+   ;;
+esac
+
+out_abi="${base_abi}${abi_suffix}"
 
 echo "${out_cpu},${out_abi}"
