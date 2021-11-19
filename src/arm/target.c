add7b434bb383caeb8ef6e9fde541322620c4bb4
Benjamin Stürz <benni@stuerz.xyz>
2021-11-19 12:54:33 +0100

    arm: Added incomplete target_info
    Signed-off-by: Benjamin Stürz <benni@stuerz.xyz>

---
+++ src/arm/target.c
@@ -0,0 +1,112 @@
+//  Copyright (C) 2021 Benjamin Stürz
+//  
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "binutils.h"
+#include "target.h"
+#include "config.h"
+
+static const char* fend_asm[]      = { "s", "S", NULL };
+static const char* fend_obj[]      = { "o", NULL      };
+static const char* fend_archive[]  = { "a", NULL      };
+static const char* fend_dll[]      = { "so", NULL     };
+
+const struct target_info target_info = {
+   .name = BCC_FULL_ARCH,
+   .size_byte     = 1,
+   .size_char     = 1,
+   .size_short    = 2,
+   .size_int      = 4,
+   .size_long     = BITS / 8,
+   .size_float    = 4,
+   .size_double   = 8,
+   .size_pointer  = BITS / 8,
+
+   .min_byte   = INT8_MIN,
+   .max_byte   = INT8_MAX,
+   .max_ubyte  = UINT8_MAX,
+   
+   .min_char   = INT8_MIN,
+   .max_char   = INT8_MAX,
+   .max_uchar  = UINT8_MAX,
+   
+   .min_short  = INT16_MIN,
+   .max_short  = INT16_MAX,
+   .max_ushort = UINT16_MAX,
+   
+   .min_int    = INT32_MIN,
+   .max_int    = INT32_MAX,
+   .max_uint   = UINT32_MAX,
+   
+#if BITS == 32
+   .min_long   = INT32_MIN,
+   .max_long   = INT32_MAX,
+   .max_ulong  = UINT32_MAX,
+#else
+   .min_long   = INT64_MIN,
+   .max_long   = INT64_MAX,
+   .max_ulong  = UINT64_MAX,
+#endif
+
+   .unsigned_char = true,
+
+   .fend_asm      = fend_asm,
+   .fend_obj      = fend_obj,
+   .fend_archive  = fend_archive,
+   .fend_dll      = fend_dll,
+
+   .ptrdiff_type  = INT_LONG,
+   .size_type     = INT_LONG,
+   .has_c99_array = false,
+
+   .size_int8 = INT_BYTE,
+   .size_int16 = INT_SHORT,
+   .size_int32 = INT_INT,
+   .size_int64 = INT_LONG,
+
+   .max_immed = 256,
+   .min_immed = 0,
+   .max_iload = INT32_MAX,
+   .min_iload = INT32_MIN,
+
+   .fuse_fp_rw = false,
+   .fuse_gl_rw = false,
+   .fuse_lu_rw = false,
+};
+
+const struct binutils_info binutils_info = {
+   .comment          = "#",
+
+   .section_text     = ".text",
+   .section_data     = ".data",
+   .section_rodata   = ".rodata",
+   .section_bss      = ".bss",
+
+   .init_byte        = ".byte",
+   .init_char        = ".byte",
+   .init_short       = ".short",
+   .init_int         = ".word",
+#if BITS == 32
+   .init_long        = ".word",
+   .init_ptr         = ".word",
+#else
+   .init_long        = ".dword",
+   .init_ptr         = ".dword",
+#endif
+   .init_float       = ".word",
+   .init_double      = ".dword",
+   .init_bool        = ".byte",
+   .init_string      = ".string",
+   .init_zero        = ".zero",
+};
